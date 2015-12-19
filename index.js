2402608fd108237825a54e795a517ca485266f33
Cole I <ci@avicora.com>
2015-12-18 19:51:36 -0500

    Added the ability to specify custom stdout values corresponding to "on" and optional substring matching.

--- index.js
+++ index.js
@@ -18,12 +18,24 @@ function SshAccessory(log, config) {
   this.onCommand = config['on'];
   this.offCommand = config['off'];
   this.stateCommand = config['state'];
+  this.onValue = config['on_value'] || "playing";
+  this.onValue = this.onValue.strip().toLowerCase();
+  this.exactMatch = config['exact_match'] || true;
   this.ssh = assign({
     user: config['user'],
     host: config['host'],
     password: config['password'],
     key: config['key']
   }, config['ssh']);
+}
+
+SshAccessory.prototype.matchesString = function(match) {
+  if(this.exactMatch) {
+    return (match === this.onValue);
+  }
+  else {
+    return (match.indexOf(this.onValue) > -1);
+  }
 }
 
 SshAccessory.prototype.setState = function(powerOn, callback) {
@@ -57,9 +69,9 @@ SshAccessory.prototype.getState = function(callback) {
   });
 
   stream.on('data', function (data) {
-    var state = data.toString('utf-8').trim();
+    var state = data.toString('utf-8').trim().toLowerCase();
     accessory.log('State of ' + accessory.name + ' is: ' + state);
-    callback(null, state === 'playing' ? true : false);
+    callback(null, accessory.matchesString(state));
   });
 }
 
